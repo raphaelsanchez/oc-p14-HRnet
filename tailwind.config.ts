401ca9a8c6b1a8a2177da415817b2f5f406f3845
Raphael Sanchez <hello@raphaelsanchez.design>
2024-07-19 00:00:01 +0200

    chore: Update @raphysan/selectbox-react dependency to version 0.1.11

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ export default {
     darkMode: ['class'],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     prefix: '',
+    safelist: ['ps-8'],
     theme: {
         container: {
             center: true,
